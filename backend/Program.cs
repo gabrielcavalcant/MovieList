2010628f80adfb0ef542688602e60256f7fedfae
Gabriel Cavalcante <gabriel_sep@outlook.com>
2024-09-01 21:48:12 -0300

    feat: update Program.cs
    Refactored Program.cs to enhance application configuration and initialization. Improved middleware setup and service registrations.

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace backend
 {
     public class Program
@@ -24,3 +26,4 @@ namespace backend
                 });
     }
 }
+
