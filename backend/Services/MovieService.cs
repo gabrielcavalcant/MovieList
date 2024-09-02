ad04d677d89f59f4c76d72a2e154f9e0b5eb900b
Gabriel Cavalcante <gabriel_sep@outlook.com>
2024-09-01 21:48:25 -0300

    feat: add Services directory
    Created Services directory to implement business logic and service classes. This organization helps separate business logic from other components of the application.

---
+++ backend/Services/MovieService.cs
@@ -0,0 +1,46 @@
+using backend.Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace backend.Services
+{
+    public class MovieService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "f87a9b269057db925af18a30c6991197";
+        private readonly string _baseUrl = "https://api.themoviedb.org/3";
+
+        public MovieService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Movie>> SearchMoviesByTitleAsync(string title)
+        {
+            var response = await _httpClient.GetAsync($"{_baseUrl}/search/movie?query={title}&api_key={_apiKey}");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(content);
+
+            var movies = new List<Movie>();
+
+            foreach (var result in json["results"])
+            {
+                movies.Add(new Movie
+                {
+                    Id = (int)result["id"],
+                    Title = (string)result["title"],
+                    Rating = (double)result["vote_average"],
+                    PosterUrl = $"https://image.tmdb.org/t/p/w500{result["poster_path"]}",
+                    Overview = (string)result["overview"],
+                    
+                });
+            }
+
+            return movies;
+        }
+    }
+}
