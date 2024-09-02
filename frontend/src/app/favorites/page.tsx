47351270d473b69ee4ddf7e65d83017b8adbd8b5
gabrielcavalcant <gabriel_sep@outlook.com>
2024-09-02 20:55:28 -0300

    feat: create FavoritesPage component
    - Added basic structure for FavoritesPage.
    - Included initial setup with flexbox for layout styling.

---
+++ frontend/src/app/favorites/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { CoinsIcon, Link } from "lucide-react";
+import React from "react";
+
+export default function FavoritesPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-start justify-start ">
+
+    </main>
+  );
+}
