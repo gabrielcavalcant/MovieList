4de93bc8abab3374847682fe7e8a42e901a1c7c4
gabrielcavalcant <gabriel_sep@outlook.com>
2024-09-02 16:23:46 -0300

    feature: update MoviesController with new endpoints and logic
    - Added new endpoints to handle movie-related requests.
    - Implemented additional logic to improve movie data management.
    - Refactored existing methods to enhance performance and maintainability.

--- backend/Controllers/MoviesController.cs
+++ backend/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using backend.Services;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace backend.Controllers
 {
@@ -13,6 +15,43 @@ namespace backend.Controllers
         public MoviesController(MovieService movieService)
         {
             _movieService = movieService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllMovies(int pageNumber = 1, int pageSize = 10)
+        {
+            // Recupera todos os filmes do serviço
+            var allMovies = await _movieService.GetAllMoviesAsync();
+
+            // Calcula o total de itens e a quantidade total de páginas
+            var totalItems = allMovies.Count();
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            // Aplica a paginação nos filmes
+            var paginatedMovies = allMovies
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // Cria a resposta com informações de paginação e filmes paginados
+            var response = new
+            {
+                TotalItems = totalItems,
+                TotalPages = totalPages,
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                Items = paginatedMovies
+            };
+
+            return Ok(response);
+        }
+
+
+        [HttpGet("popular")]
+        public async Task<IActionResult> GetPopularMovies()
+        {
+            var movies = await _movieService.GetPopularMoviesAsync();
+            return Ok(movies);
         }
 
         [HttpGet("search")]
