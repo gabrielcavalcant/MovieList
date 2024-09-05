e3aaa3d5bb63f7edb2eefb4d13a397b36074fb7e
gabrielcavalcant <gabriel_sep@outlook.com>
2024-09-04 21:52:37 -0300

    feat: add FavoriteList model for database schema
    - Created FavoriteList model to represent favorite lists in the database.
    - Defined properties and data annotations for the model.
    - Ensured compatibility with Entity Framework for migrations and database operations.

---
+++ backend/Models/FavoriteList.cs
@@ -0,0 +1,11 @@
+﻿//namespace backend.Models
+//{
+//    public class FavoriteList
+//    {
+//        public int FavoriteId { get; set; }
+//        public Favorite Favorite { get; set; }
+
+//        public int ListId { get; set; }
+//        public List List { get; set; }
+//    }
+//}
