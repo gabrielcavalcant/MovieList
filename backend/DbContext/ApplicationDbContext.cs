de1a489c866bd25058a43410391ad26d331ac780
gabrielcavalcant <gabriel_sep@outlook.com>
2024-09-04 21:52:25 -0300

    feat: configure ApplicationDbContext for database interaction
    - Set up ApplicationDbContext to handle database connections and transactions.
    - Configured DbContext to use SQL Server or another specified database provider.
    - Registered DbContext in Startup.cs for dependency injection.

---
+++ backend/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<List> Lists { get; set; }
+        //public DbSet<FavoriteList> FavoriteLists { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<FavoriteList>()
+        //        .HasKey(fl => new { fl.FavoriteId, fl.ListId });
+
+        //    modelBuilder.Entity<FavoriteList>()
+        //        .HasOne(fl => fl.Favorite)
+        //        .WithMany(f => f.FavoriteLists)
+        //        .HasForeignKey(fl => fl.FavoriteId);
+
+        //    modelBuilder.Entity<FavoriteList>()
+        //        .HasOne(fl => fl.List)
+        //        .WithMany(l => l.FavoriteLists)
+        //        .HasForeignKey(fl => fl.ListId);
+        //}
+    }
+}
